9c9a56ca6fc4057710efcb924015e28cdab31b13
zunbin <905192187@qq.com>
2020-03-07 21:45:57 +0800

    重构完成

--- wzb-security/wzb-security-browser/src/main/java/com/wzb/browser/BrowserSecurityConfig.java
+++ wzb-security/wzb-security-browser/src/main/java/com/wzb/browser/BrowserSecurityConfig.java
@@ -1,19 +1,18 @@
 package com.wzb.browser;
 
-import com.wzb.browser.authentication.WzbAuthenctiationFailureHandler;
-import com.wzb.browser.authentication.WzbAuthenticationSuccessHandler;
+import com.wzb.security.core.authentication.AbstractChannelSecurityConfig;
 import com.wzb.security.core.authentication.mobile.SmsCodeAuthenticationSecurityConfig;
+import com.wzb.security.core.properties.SecurityConstants;
 import com.wzb.security.core.properties.SecurityProperties;
 import com.wzb.security.core.validate.code.ValidateCodeFilter;
+import com.wzb.security.core.validate.code.ValidateCodeSecurityConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
 import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
 
@@ -28,22 +27,19 @@ import javax.sql.DataSource;
  */
 
 @Configuration
-public class BrowserSecurityConfig extends WebSecurityConfigurerAdapter {
+public class BrowserSecurityConfig extends AbstractChannelSecurityConfig {
 
     @Autowired
     private SecurityProperties securityProperties;
-
-    @Autowired
-    private WzbAuthenticationSuccessHandler wzbAuthenticationSuccessHandler;
-
-    @Autowired
-    private WzbAuthenctiationFailureHandler wzbAuthenctiationFailureHandler;
 
     @Autowired
     private ValidateCodeFilter validateCodeFilter;
 
     @Autowired
     private UserDetailsService userDetailsService;
+
+    @Autowired
+    private ValidateCodeSecurityConfig validateCodeSecurityConfig;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -77,47 +73,67 @@ public class BrowserSecurityConfig extends WebSecurityConfigurerAdapter {
      */
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-
-
-        http// 表单登陆
-                .addFilterBefore(validateCodeFilter, UsernamePasswordAuthenticationFilter.class)
-                //这个是去到一个页面, 输入用户名和密码
-                .formLogin()
-                //告诉spring先跳到这个页面
-                .loginPage("/authentication/require")
-                // 告诉spring security去/authentication/require"再去UsernamePasswordAuthenticationFilter认证
-                .loginProcessingUrl("/authentication/form")
-                // 登录成功
-                .successHandler(wzbAuthenticationSuccessHandler)
-                // 登录失败
-                .failureHandler(wzbAuthenctiationFailureHandler)
-                // 这个是弹框输入用户名和密码
-                //                .httpBasic()
+        applyPasswordAuthenticationConfig(http);
+        http.apply(validateCodeSecurityConfig)
                 .and()
-                // 记住我的功能(RememberMeAuthenticationFilter: 当前面的过滤器没有效果不行的时候, 它会调用此过滤器)
+                .apply(smsCodeAuthenticationSecurityConfig)
+                    .and()
                 .rememberMe()
-                .tokenRepository(persistentTokenRepository())
-                .tokenValiditySeconds(securityProperties.getBrowser().getRememberMeSeconds())
-                .userDetailsService(userDetailsService)
-                .and()
-                // 对下面请求的授权
+                    .tokenRepository(persistentTokenRepository())
+                    .tokenValiditySeconds(securityProperties.getBrowser().getRememberMeSeconds())
+                    .userDetailsService(userDetailsService)
+                    .and()
                 .authorizeRequests()
-                // 下面的地址不需要身份认证
-                // 下面这个error要加上, 不然这个页面会要求认证
-                .antMatchers("/authentication/require",
-                        "/error",
-                        "/code/image",
-                        "/code/sms",
-                        securityProperties.getBrowser().getLoginPage()
-                ).permitAll()
-                // 剩下任何请求都需要认证
-                .anyRequest()
-                //  都需要身份认证
-                .authenticated()
-                .and()
-                // 暂时把跨域关掉
-                .csrf().disable()
-                .apply(smsCodeAuthenticationSecurityConfig);
+                    .antMatchers(
+                            SecurityConstants.DEFAULT_UNAUTHENTICATION_URL,
+                            SecurityConstants.DEFAULT_LOGIN_PROCESSING_URL_MOBILE,
+                            securityProperties.getBrowser().getLoginPage(),
+                            SecurityConstants.DEFAULT_VALIDATE_CODE_URL_PREFIX+"/*")
+                    .permitAll()
+                    .anyRequest()
+                    .authenticated()
+                    .and()
+                .csrf().disable();
+
+
+        /************  第二版本 start  ************/
+//        http// 表单登陆
+//                .addFilterBefore(validateCodeFilter, UsernamePasswordAuthenticationFilter.class)
+//                //这个是去到一个页面, 输入用户名和密码
+//                .formLogin()
+//                //告诉spring先跳到这个页面
+//                .loginPage("/authentication/require")
+//                // 告诉spring security去/authentication/require"再去UsernamePasswordAuthenticationFilter认证
+//                .loginProcessingUrl("/authentication/form")
+//
+//                // 这个是弹框输入用户名和密码
+//                //                .httpBasic()
+//                .and()
+//                // 记住我的功能(RememberMeAuthenticationFilter: 当前面的过滤器没有效果不行的时候, 它会调用此过滤器)
+//                .rememberMe()
+//                .tokenRepository(persistentTokenRepository())
+//                .tokenValiditySeconds(securityProperties.getBrowser().getRememberMeSeconds())
+//                .userDetailsService(userDetailsService)
+//                .and()
+//                // 对下面请求的授权
+//                .authorizeRequests()
+//                // 下面的地址不需要身份认证
+//                // 下面这个error要加上, 不然这个页面会要求认证
+//                .antMatchers("/authentication/require",
+//                        "/error",
+//                        "/code/image",
+//                        "/code/sms",
+//                        securityProperties.getBrowser().getLoginPage()
+//                ).permitAll()
+//                // 剩下任何请求都需要认证
+//                .anyRequest()
+//                //  都需要身份认证
+//                .authenticated()
+//                .and()
+//                // 暂时把跨域关掉
+//                .csrf().disable()
+//                .apply(smsCodeAuthenticationSecurityConfig);
+    /************  第二版本  end  ************/
 
 //        第一版本
    /*     http// 表单登陆
