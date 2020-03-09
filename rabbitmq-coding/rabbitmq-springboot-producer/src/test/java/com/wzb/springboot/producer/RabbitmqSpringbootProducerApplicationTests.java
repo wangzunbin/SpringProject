2053b33e71c01af383b8c52a1c0b730d4152c84c
zunbin <905192187@qq.com>
2020-03-09 20:54:07 +0800

    完成springboot的生产模块和消费模块的初步配置

---
+++ rabbitmq-coding/rabbitmq-springboot-producer/src/test/java/com/wzb/springboot/producer/RabbitmqSpringbootProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wzb.springboot.producer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitmqSpringbootProducerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
